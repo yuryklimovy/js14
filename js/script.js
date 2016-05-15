43e85878aefbacfdf1f650b221714c0b0bc8eba3
Iurii Klymov <klymovyury@ukr.net>
2016-05-15 16:41:17 +0300

    js14

--- js/script.js
+++ js/script.js
@@ -1,91 +1,86 @@
-var createObj = {
-  questions : [
-    {
-      title : 'Вопрос №1',
-      answers : [
-        {text : 'Ответ №1',
-        correct: false},
-        {text : 'Ответ №2',
-        correct: false},
-        {text : 'Ответ №3',
-        correct: true}
-      ]
-    },
-    {
-      title : 'Вопрос №2',
-      answers : [
-        {text : 'Ответ №1',
-        correct: false},
-        {text : 'Ответ №2',
-        correct: false},
-        {text : 'Ответ №3',
-        correct: true}
-      ]
-    },
-    {
-      title : 'Вопрос №3',
-      answers : [
-        {text : 'Ответ №1',
-        correct: false},
-        {text : 'Ответ №2',
-        correct: false},
-        {text : 'Ответ №3',
-        correct: true}
-      ]
-    },
-  ],
+$(function() {
+  'use strict';
 
-  createEl : function(tagName, className, text = ''){
-    var newElement = document.createElement(tagName);
-    newElement.className = className;
-    newElement.innerHTML = text;
-    return(newElement);
-  },
-
-  createChecbox : function(id){
-    var newElement = document.createElement('input');
-    newElement.className = 'checkbox';
-    newElement.setAttribute('id', id);
-    newElement.setAttribute('type','checkbox');
-    return(newElement);
-  },
-
-  createLabel : function(className, checkBoxId, text = ''){
-    var newElement = document.createElement('label');
-    newElement.className = className;
-    newElement.setAttribute('for', checkBoxId);
-    newElement.innerHTML = text;
-    return(newElement);
-  },
-  createButton : function(className, parrentTag = 'body', text = ''){
-    var newElement = document.createElement('input');
-    newElement.className = className;
-    newElement.setAttribute('type','submit');
-    newElement.setAttribute('value', text);
-    var newButton = document.getElementsByTagName(parrentTag);
-    newButton[0].appendChild(this.createEl('div', 'buttonWraper'));
-    newButton = document.getElementsByClassName('buttonWraper');
-    newButton[0].appendChild(newElement);
-  },
-  generateQuestions : function(parrentTag = 'body'){
-    var parrent = document.getElementsByTagName(parrentTag);
-    for (var i = 0; i < this.questions.length; i++) {
-      var newQuestion;
-      newQuestion = this.createEl('div', 'questionsWraper');
-      parrent[0].appendChild(newQuestion);
-      newQuestion.appendChild(this.createEl('h3', 'question', this.questions[i].title));
-      for (var j = 0; j < this.questions[i].answers.length; j++) {
-        var newAnswer = this.createEl('div', 'answerWraper');
-        newQuestion.appendChild(newAnswer);
-        newAnswer.appendChild(this.createChecbox('check'+i+j));
-        newAnswer.appendChild(this.createLabel('label', 'check'+i+j, this.questions[i].answers[j].text));
-    }
+  var test = {
+    questions: [{
+      title: 'Вопрос 1',
+      answers: [{
+        text: 'Ответ №11',
+        correct: true
+      }, {
+        text: 'Ответ №12',
+        correct: false
+      }, {
+        text: 'Ответ №13',
+        correct: false
+      }]
+    }, {
+      title: 'Вопрос 2',
+      answers: [{
+        text: 'Ответ №21',
+        correct: false
+      }, {
+        text: 'Ответ №22',
+        correct: false
+      }, {
+        text: 'Ответ №23',
+        correct: true
+      }, {
+        text: 'Ответ №24',
+        correct: true
+      }]
+    }, {
+      title: 'Вопрос 3',
+      answers: [{
+        text: 'Ответ №31',
+        correct: false
+      }, {
+        text: 'Ответ №32',
+        correct: true
+      }, {
+        text: 'Ответ №33',
+        correct: false
+      }]
+    }]
   }
-}
-};
+
+  localStorage.setItem('test', JSON.stringify(test));
 
 
-document.body.appendChild(createObj.createEl('h2','headLine','Тест по программированию'));
-document.body.appendChild(createObj.createEl('form','testForm'));
-createObj.generateQuestions('form');
-createObj.createButton('button','form', 'Проверить мои результаты');
+  var html = $('#tmpl').html();
+  var data = JSON.parse(localStorage.getItem('test'));
+  var content = tmpl(html, data);
+
+
+  $('.buttonWraper').before(content);
+
+  $('.button').click(function() {
+    var id = '#check';
+    var answers = [];
+    var result = [];
+    var rigthAnswers = 0;
+
+    for (var i = 0; i < data.questions.length; i++) {
+      result[i] = 0;
+      for (var j = 0; j < data.questions[i].answers.length; j++) {
+
+        if ($(id + i + j)[0].checked == data.questions[i].answers[j].correct) {
+          result[i] = result[i] + 1;
+        }
+      }
+    }
+    for (var i = 0; i < result.length; i++) {
+      if (result[i] == data.questions[i].answers.length) {
+        rigthAnswers++
+      }
+    }
+    console.log(rigthAnswers);
+    $('.modal-window').html('Вы правильно ответили на ' + rigthAnswers);
+    $('.modal').addClass('modal-active');
+    $('.modal-window').addClass('modal-window-active');
+  })
+  $('.modal').click(function(){
+    $('.modal-window').removeClass('modal-window-active');
+    $('.modal').removeClass(' modal-active')
+  })
+});
